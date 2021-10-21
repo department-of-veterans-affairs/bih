019778cddb6de814de048cac4ed176e57bfbd982
Sheppard <Brandon.Sheppard@va.gov>
2021-10-21 11:38:43 -0400

    removed entrypoint script

--- scripts/entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# app container entrypoint script for starting envconsul as
-# a sub-process then running the application
-
-/bin/envconsul -consul-addr=${CONSUL_ADDR} -consul-token=${CONSUL_TOKEN} -prefix="config/bih/development" node packages/backend --config app-config.yaml
