a6a32440d4ba40faa73a5ab10370c0e0f45e7c21
Aaron Wise <Wisertown@users.noreply.github.com>
2021-11-02 17:47:20 -0400

    removed console.log

--- plugins/dashboard/src/hooks/useDashboard.ts
+++ plugins/dashboard/src/hooks/useDashboard.ts
@@ -4,7 +4,6 @@ import useAsync from 'react-use/lib/useAsync';
 
 export function useDashboard() {
     const dashboardApi = useApi(dashboardApiRef);
-    console.log(dashboardApi);
     const response = useAsync(() => 
         dashboardApi.getDashboardData(), []
     );
